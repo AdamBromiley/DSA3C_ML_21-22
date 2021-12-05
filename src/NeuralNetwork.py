4aafcd977eaf29ff5a4d92fd7388a87f3d2c6592
AdamBromiley <adam@bromiley.co.uk>
2021-12-05 00:06:07 +0000

    create __init__ and propagate methods

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -3,10 +3,25 @@ from Layer import Layer
 
 class NeuralNetwork:
     def __init__(self, weights, biases, activiation_functions):
-        return
+        layer_count = len(weights)
+
+        if (len(biases) != layer_count
+                or len(activiation_functions) != layer_count):
+            raise ValueError
+
+        self.layers = []
+
+        for i in range(0, layer_count):
+            layer = Layer(weights[i], biases[i], activiation_functions[i])
+            self.layers.append(layer)
 
     def propagate(self, nn_inputs):
-        return
+        inputs = nn_inputs
+
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+
+        return inputs
 
     def getLayers(self):
-        return
+        return self.layers
